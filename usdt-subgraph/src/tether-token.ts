b597bec1d282313763529b3f0022824375517d24
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2022-10-08 17:38:41 +0530

    yaml
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

--- usdt-subgraph/src/tether-token.ts
+++ usdt-subgraph/src/tether-token.ts
@@ -1,7 +1,9 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import { Approval as ApprovalEvent, Transfer as TransferEvent } from "../generated/TetherToken/TetherToken"
+import {UserAccount, Transaction} from "../generated/schema"
 
-
-export function handleApproval(event: ApprovalEvent): void { }
+export function handleApproval(event: ApprovalEvent): void {
+    
+}
 
 export function handleTransfer(event: TransferEvent): void { }
